d54fca5d04ccef35e43e2a0801b99bf1af08e4e4
Camilly Gama <camysgama@outlook.com>
2022-01-19 21:19:57 -0300

    refactor: create category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,28 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { Category } from '../model/Category';
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
-
-categoriesRoutes.post('/', (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = new Category();
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date(),
-    });
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category Already exists!" });
+    }
 
-    categories.push(category);
+    categoriesRepository.create({ name, description });
 
-    return response.status(201).json({ category });
+    return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 });
 
 export { categoriesRoutes };
