32eb95dd0e4b918aed6f7da7ce8695826d44dff3
Camilly Gama <camysgama@outlook.com>
2022-02-24 21:13:37 -0300

    feat: user controller

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+@injectable()
 class CreateUserUseCase {
     constructor(
         @inject("UsersRepository")
-        private usersRepository: IUsersRepository,
+        private usersRepository: IUsersRepository
     ) { }
 
     async execute({
